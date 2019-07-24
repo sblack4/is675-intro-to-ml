ea87fba9e0a272629f0c69a846282b407ae180e8
Steven Black <genseb7@gmail.com>
2019-07-23 20:09:18 -0400

    hw7 wip

--- Homework/07/Homework.py
+++ Homework/07/Homework.py
@@ -8,7 +8,7 @@
 # 
 # 
 # 1. Import appropriate libraries to cluster iris data using k-means. (10 points)
-# 2. Load the iris data: first 4 attributes into X and the class attribute into y. (10 points)
+# 2. Load the iris data: first 4 attributes into data_tuple and the class attribute into y. (10 points)
 # 3. Create 3 clusters using K-means algorithm. (10 points)
 # 4. Save the centroids of each cluster into a variable. (10 points)
 # 5. Plot (2D) the data points with color-coded clusters along with the centroids using any two features. Which two features show the best cluster separation? (30 points)
@@ -30,16 +30,16 @@ from sklearn.metrics.cluster import silhouette_score,silhouette_samples
 import matplotlib.pyplot as plt
 
 
-# ## 2. Load the iris data: first 4 attributes into X and the class attribute into y. (10 points)
+# ## 2. Load the iris data: first 4 attributes into data_tuple and the class attribute into y. (10 points)
 
 # In[3]:
 
 
 iris_dataset = datasets.load_iris()
 
-X = iris_dataset.data[:,:4]
+data_tuple = iris_dataset.data[:,:4]
 y = iris_dataset.target
-print('Data Shape: ', X.shape, ' Target Shape: ', y.shape)
+print('Data Shape: ', data_tuple.shape, ' Target Shape: ', y.shape)
 
 
 # ## 3. Create 3 clusters using K-means algorithm. (10 points)
@@ -48,7 +48,7 @@ print('Data Shape: ', X.shape, ' Target Shape: ', y.shape)
 
 
 kmeans_model = KMeans(n_clusters=3)
-kmeans_model.fit(X, y)
+kmeans_model.fit(data_tuple, y)
 
 
 # ## 4. Save the centroids of each cluster into a variable. (10 points)
@@ -63,32 +63,36 @@ centroids
 
 # ## 5. Plot (2D) the data points with color-coded clusters along with the centroids using any two features. Which two features show the best cluster separation? (30 points)
 
+# In[]:
+fig = plt.figure()
+help(fig)
 # In[19]:
 
 
+def plot__kmeans_scatter_in_2d(data_tuple):
+    colors = ["r.","g.","b.","c.","y.","k."]
+
+    print(data_tuple)
+    fig = plt.figure()
+    fig.title(lable=f'{data_tuple[:][0]}, "vs", {data_tuple[:][1]}')
+
+    for i in range(len(data_tuple)):
+        plt.plot(data_tuple[i][0], data_tuple[i][1], colors[labels[i]], markersize = 7)
+    plt.scatter(centroids[:,0], centroids[:,1], marker='x', color='k', s=50, linewidth=5)
+    plt.show()
+
+
 pairs = itertools.combinations(centroids, 2)
-pairs.
 
-
-# In[ ]:
-
-
-def plot__kmeans_scatter_in_2d():
-    
-colors = ["r.","g.","b.","c.","y.","k."]
-
-for i in range(len(X)):
-    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize = 7)
-plt.scatter(centroids[:,0], centroids[:,1], marker='x', color='k', s=50, linewidth=5)
-plt.show()
-
+for item in pairs:
+    plot__kmeans_scatter_in_2d(item)
 
 # ## 6. Calculate the silhouette coefficients. (10 points)
 
 # In[9]:
 
 
-silhouette_score(X, kmeans_model.labels_, metric='sqeuclidean')
+silhouette_score(data_tuple, kmeans_model.labels_, metric='sqeuclidean')
 
 
 # ## 7. Assuming that the data is ordered by class labels, print the average silhouette coefficient for each class. (20 points)
